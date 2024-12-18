f30906dd26833d0dbe9044c2f5a6538c72c47c1b
leonardomb1 <lmbaudin2013@gmail.com>
2024-12-18 08:09:46 -0300

    Fix: Program.cs invalid import

--- src/Program.cs
+++ src/Program.cs
@@ -2,19 +2,20 @@
 namespace ITCentral;
 
 public class Program
-{   
+{
     public static void Main(string[] args)
     {
-        Action switcher = args.FirstOrDefault() switch {
+        Action switcher = args.FirstOrDefault() switch
+        {
             string o when o == "-h" || o == "--help" => AppCommon.ShowHelp,
             string o when o == "-v" || o == "--version" => AppCommon.ShowVersion,
-            string o when o == "-e" || o == "--environment" => AppCommon.InitializeFromEnv,
-            string o when o == "-f" || o == "--file" => () => AppCommon.InitializeFromYaml(args.ElementAt(1)),
-            string o when o == "-c" || o == "--console" => () => AppCommon.InitializeFromArgs([ .. args.Skip(1) ]),
-            null => AppCommon.InitializeFromEnv,
-            _ => () => {Console.WriteLine("This option is invalid."); AppCommon.ShowHelp();}
+            string o when o == "-e" || o == "--environment" => Initializer.InitializeFromEnv,
+            string o when o == "-f" || o == "--file" => () => Initializer.InitializeFromYaml(args.ElementAt(1)),
+            string o when o == "-c" || o == "--console" => () => Initializer.InitializeFromArgs([.. args.Skip(1)]),
+            null => Initializer.InitializeFromEnv,
+            _ => () => { Console.WriteLine("This option is invalid."); AppCommon.ShowHelp(); }
         };
-        
+
         switcher.Invoke();
     }
 }
